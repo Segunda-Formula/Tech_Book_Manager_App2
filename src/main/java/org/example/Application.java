79155451b493a5131a1f52b569b99c8765d3b98a
Andreea <andreea.celmare@gmail.com>
2024-10-30 09:36:38 +0100

    build: remove .git

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Application {
+    public void printMenu() {
+        Scanner sc = new Scanner(System.in);
+
+        int opcion = 0;
+        do {
+            java.lang.System.out.println("\nGestor de Libros Técnicos de Programación\n 1. Añadir libro\n 2. Ver todos los libros\n 3. Eliminar libro\n 4. Cambiar repositorio\n 5. Salir\n Seleccione una opción: ");
+
+            java.lang.String input = sc.nextLine();
+
+            try {
+                opcion = java.lang.Integer.parseInt(input);
+            } catch (java.lang.Exception ex) {
+                java.lang.System.out.println("Opción inválida. Por favor, introduce un número del 1 al 5.");
+                continue;
+            }
+
+            if (opcion < 1 || opcion > 5) {
+                java.lang.System.out.println("Opción inválida. Por favor, seleccione una opción del 1 al 5.");
+            }
+            switch (opcion) {
+                case 1:
+                    java.lang.System.out.println("Opción 1: Añadir libro");
+                    break;
+
+                case 2:
+                    java.lang.System.out.println("Opción 2: Ver todos los libros");
+                    break;
+
+                case 3:
+                    java.lang.System.out.println("Opción 3: Eliminar libro");
+                    break;
+
+                case 4:
+                    java.lang.System.out.println("Opción 4: Cambiar repositorio");
+                    break;
+
+                case 5:
+                    java.lang.System.out.println("Opción 5: Salir");
+                    break;
+
+            }
+        } while (opcion != 5);
+    }
+}

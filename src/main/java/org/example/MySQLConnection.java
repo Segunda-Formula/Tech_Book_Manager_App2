8f0b651c99470bbd1e4b83f95ced2e7160a72d76
monica <mosice@gmail.com>
2024-11-11 15:12:12 +0100

    feat: class MySQLConnection created

---
+++ src/main/java/org/example/MySQLConnection.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+    private static String url = "jdbc:mysql://localhost:3306/my_database";
+    private static String user = "root";
+    private static String password = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+}

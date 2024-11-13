22dfa38989409688d8667bb44091ca8e6506da17
Paola Perdomo <paolaperdomo079@gmail.com>
2024-11-13 11:52:18 +0100

    feat: Create class InMemoryRepository and implement The interface BookRepository

---
+++ src/main/java/org/example/InMemoryRepository.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class InMemoryRepository implements BookRepository{
+    private static List<Book> booksDB = new ArrayList<>();
+
+    @Override
+    public void save(Book book) {
+        booksDB.add(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return booksDB;
+    }
+
+    @Override
+    public Optional<Book> findByIsbn(String isbn) {
+        for (Book book : booksDB) {
+            if(book.getIsbn().equals(isbn)) {
+                return Optional.of(book);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteByIsbn(String isbn) {
+        booksDB.removeIf(book -> book.getIsbn().equals(isbn));
+    }
+}
